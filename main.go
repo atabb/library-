011744bd2096ee4754db79f5338181b0ee1f69a9
Abdul T <atabbakha95@gmail.com>
2022-02-19 10:43:51 -0600

    update

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -42,11 +43,48 @@ func addBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, addBooks) //return book we created with status created to let us know it worked!
 }
 
+func bookById(c *gin.Context) {
+	id := c.Param("id")          //In this case we will set parameter to ID
+	book, err := getBookById(id) //must include err since we did in function of getbookbyID
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found."}) //allows us to return custom error after not finding book.
+		return
+	}
+
+	c.IndentedJSON(http.StatusAccepted, book)
+}
+
+func getBookById(id string) (*book, error) { //if book doesn't exist, returns error.
+	for i, b := range books { //for loop through all books which global struct
+		if b.ID == id { //if theres a hit, return the pointer to the book
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found") // else returns book not found
+}
+
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	if ok == false {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"messgae": "Missing id query parameter"})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.in
+	}
+
+}
+
 func main() {
 
 	router := gin.Default()        //create router gin
 	router.GET("/books", getBooks) //if you visit /books site, it will return function getbooks define above.
 	router.POST("/books", addBooks)
+	router.GET("/books/:id", bookById) //set up the paramter which i defined in function getbookbyID
 	router.Run("localhost:8080")
 
 }
